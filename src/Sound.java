44e5d6288d0eb8a478291e9f71f6ecdde32817ef
Jay T <torrcorp@gmail.com>
2018-05-26 16:14:09 -0600

    Initial commit

---
+++ src/Sound.java
@@ -0,0 +1,17 @@
+/*
+  Try This 2-1
+  Compute the distance to a lighting
+  strike whose sound takes 7.2 seconds
+  to reach you.
+ */
+
+ public class Sound {
+  public static void main(String args[]) {
+    double dist;
+
+    dist = 7.2 * 1100;
+
+    System.out.println("the Lightning is " + dist + " feet away.");
+    
+  }
+}
